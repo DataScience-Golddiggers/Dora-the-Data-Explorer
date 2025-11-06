b4d3ceccfe3916a693d7d89e4a8a0100de2586ed
Gabriele Vianello <vianello.tech@gmail.com>
2025-11-06 18:46:41 +0100

    sync

--- src/inference/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Inference modules."""
-
-from .inference_pipeline import ModelInference, compare_models, batch_inference
-
-__all__ = [
-    'ModelInference',
-    'compare_models',
-    'batch_inference'
-]
